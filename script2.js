adb6caa606522f084d908cc4ef6e025f06831df9
Joona Yoon <joonas-yoon@users.noreply.github.com>
2020-10-08 11:33:56 +0900

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var btn = document.getElementById('1');
+  btn.addEventListener(
+    "click",
+    () => {
+      window.alert('hi');
+    }
+  );
+});
